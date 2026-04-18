845a5435ac558b9a7c8bc50df75f049c22286091
M K Chandan <mkchandan2005@gmail.com>
2026-04-18 23:19:48 +0530

    Remove Django admin registration for MongoEngine Edition model

--- artifacts/epaper/newspaper/admin.py
+++ artifacts/epaper/newspaper/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import Edition
 
-
-@admin.register(Edition)
-class EditionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'edition_date', 'is_published', 'uploaded_by', 'created_at']
-    list_filter = ['is_published', 'edition_date']
-    search_fields = ['title', 'description']
-    date_hierarchy = 'edition_date'
+# MongoEngine models cannot be registered with Django admin
+# Edition is now a MongoEngine Document and is managed through custom views
+# See newspaper/views.py for admin_dashboard() and edition_upload/edit/delete views
